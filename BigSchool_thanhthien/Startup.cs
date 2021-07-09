bdfd27c1749bcb8a4e1c4661c30b3dd0256b87e2
DELL <DELL@DESKTOP-F7ROAS6>
2021-07-09 12:46:08 +0700

    Add project files.

---
+++ BigSchool_thanhthien/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSchool_thanhthien.Startup))]
+namespace BigSchool_thanhthien
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ BigSchool_thanhthien/ViewModels/CourseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using BigSchool_thanhthien.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool_thanhthien.ViewModels
+{
+    public class CourseViewModel
+    {
+       
+        public string Place { get; set; }
+
+       
+        public string Date { get; set; }
+
+        
+        public string Time { get; set; }
+
+       
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+
+        public DateTime GetDateTime() 
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
